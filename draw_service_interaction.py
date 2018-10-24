48b54e03715e8fe2f36633a57251951a58e975b1
vinay-kumar9 <vinay.kumar@knoldus.in>
2018-10-24 17:26:14 +0530

    adding only parsing logic in the script

--- draw_service_interaction.py
+++ draw_service_interaction.py
@@ -13,52 +13,28 @@ def main(args):
     return
 
 
-def checkCommandStatus(command_status):
-    if command_status is not 0:
-        logging.error("==Not able to connect to the cluster. check vpn/credentials")
-        exit(1)
-    else:
-        return
-
-
 def getLatestConfiguration(args):
     if not os.path.exists(args.credentials):
         logging.warning('Location of credentials doesn\'t exist.'
                         ' Create credentials.json file')
         return
     else:
-        script_dir = os.path.dirname(__file__)
-        # print script_dir
-        credentials_file_loc = os.path.join(script_dir, args.credentials)
-        # print credentials_file_loc
-        with open(credentials_file_loc, 'r') as credentials:
-            credentials = json.load(credentials)
-            # print credentials
-            logging.info("==Trying to connect to dcos cluster "
-                         + args.dcos_cluster_name)
-            command = clusterAttach.substitute(dcos_cluster_name=args.dcos_cluster_name)
-            checkCommandStatus(subprocess.call(command.split(' ')))
-
-            command = authLogin.substitute(username=credentials['sahil.sawhney']['username'],
-                                           password=credentials['sahil.sawhney']['password'])
-            checkCommandStatus(subprocess.call(command.split(' ')))
-
-            logging.info("==Getting the marathon json from cluster")
-            command_out = subprocess.check_output(getConfig.split(' '))
-            outFile_conf = open('conf/service.json', 'w')
-            outFile_conf.write(command_out)
-            outFile_conf.close()
-            logging.info("==json is now available at conf/service.json.")
+        logging.info("==Getting the marathon json from cluster")
+        command_out = subprocess.check_output(getConfig.split(' '))
+        outFile_conf = open('conf/service.json', 'w')
+        outFile_conf.write(command_out)
+        outFile_conf.close()
+        logging.info("==json is now available at conf/service.json.")
 
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Python service visualization engine')
-    parser.add_argument('-c', '--credentials', type=str, required=True,
-                        help='location of user credentials file required to login.')
-    parser.add_argument('-u', '--dcos_cluster_name', type=str, required=True,
-                        help='Cluster whose service interaction visualization'
-                             ' are required')
-    args = parser.parse_args()
+    # parser.add_argument('-c', '--credentials', type=str, required=True,
+    #                     help='location of user credentials file required to login.')
+    # parser.add_argument('-u', '--dcos_cluster_name', type=str, required=True,
+    #                     help='Cluster whose service interaction visualization'
+    #                          ' are required')
+    # args = parser.parse_args()
     return args
 
 
--- initiate_visual.sh
+++ initiate_visual.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-dcos_cluster_name=Jupiter
-username=sahil.sawhney@hpe.com
-password=KnoldusTemp5555
 
+source ./conf/_environment
+
+echo "Trying to attach to $dcos_cluster_name.."
 dcos cluster attach $dcos_cluster_name
-dcos auth login --username=$username --password=$password
-dcos marathon app list --json
 
-python draw_service_interaction.py -c credentials.json -u jupiter  
-    
+echo "Trying to login to $dcos_cluster_name..."
+dcos auth login --username=$username --password=$password
+
+python draw_service_interaction.py -u jupiter
